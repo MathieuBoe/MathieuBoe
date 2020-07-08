e6ec4100a038f32db8e281a0731d240edb03f36c
MathieuBoe <mathieuboe60@gmail.com>
2020-07-08 16:48:41 +0200

    Skills et Experience contenu créer

--- project.php
+++ project.php
@@ -1,4 +1,25 @@
 <?php include 'header.php' ?>
-4
-    
+<h1>PORTFOLIO</h1>
+<div id="portfolio">
+    <div class="website">
+        <img class="webimg" src="assets/img/hanamisushi.png" alt="">
+        <h2 class="describefolio">
+            Site réalisé en Bootstrap à rendre responsive et à améliorer
+        </h2>
+    </div>
+
+    <div class="website">
+        <img class="webimg" src="assets/img/bulleandco.png" alt="">
+        <h2 class="describefolio">
+            Site réalisé en javascript avec panier fonctionnel
+        </h2>
+    </div>
+    <div class="website">
+        <img class="webimg" src="assets/img/ocordo.png" alt="">
+        <h2 class="describefolio">
+            Reprise d'un code XML à afficher en php et à améliorer sans toucher au code XML
+        </h2>
+    </div>
+</div>
+
 <?php include 'footer.php'; ?>
