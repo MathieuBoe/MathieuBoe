ab94c75fc746ed37f680460b533ed4484c588853
MathieuBoe <mathieuboe60@gmail.com>
2020-07-20 10:55:33 +0200

    rendu avec animation pour site web, responsive a travailler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,52 @@
 // Skills Bars
-$('.skill-percent').each(function(){
-    $(this).animate({
-      width:$(this).attr('data-percent')},"fast");
-    });
+$('.skill-percent').each(function () {
+  $(this).animate({
+    width: $(this).attr('data-percent')
+  }, "fast");
+});
 
 // play video loisir overwatch
 $(document).ready(function () {
   $(".game").hover(function () {
-      $(this).children("video")[0].play();
+    $(this).children("video")[0].play();
   }, function () {
-      var el = $(this).children("video")[0];
-      el.pause();
-      el.currentTime = 0;
+    var el = $(this).children("video")[0];
+    el.pause();
+    el.currentTime = 0;
   });
 });
 
 // contact form
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	//material contact form
-	$('.contact-form').find('.form-control').each(function() {
-	  var targetItem = $(this).parent();
-	  if ($(this).val()) {
-		$(targetItem).find('label').css({
-		  'top': '10px',
-		  'fontSize': '14px'
-		});
-	  }
-	})
-	$('.contact-form').find('.form-control').focus(function() {
-	  $(this).parent('.input-block').addClass('focus');
-	  $(this).parent().find('label').animate({
-		'top': '10px',
-		'fontSize': '14px'
-	  }, 300);
-	})
-	$('.contact-form').find('.form-control').blur(function() {
-	  if ($(this).val().length == 0) {
-		$(this).parent('.input-block').removeClass('focus');
-		$(this).parent().find('label').animate({
-		  'top': '25px',
-		  'fontSize': '18px'
-		}, 300);
-	  }
-	})
-	
+  //material contact form
+  $('.contact-form').find('.form-control').each(function () {
+    var targetItem = $(this).parent();
+    if ($(this).val()) {
+      $(targetItem).find('label').css({
+        'top': '10px',
+        'fontSize': '14px'
+      });
+    }
+  })
+  $('.contact-form').find('.form-control').focus(function () {
+    $(this).parent('.input-block').addClass('focus');
+    $(this).parent().find('label').animate({
+      'top': '10px',
+      'fontSize': '14px'
+    }, 300);
+  })
+  $('.contact-form').find('.form-control').blur(function () {
+    if ($(this).val().length == 0) {
+      $(this).parent('.input-block').removeClass('focus');
+      $(this).parent().find('label').animate({
+        'top': '25px',
+        'fontSize': '18px'
+      }, 300);
+    }
+  })
+
 });
 
 // picme puffin
@@ -55,18 +56,18 @@ if (wrapper) wrapper.classList.add('magictime', 'puffIn')
 
 
 const containerleft = document.querySelectorAll('.container.left')
-if (containerleft) 
-{
-  containerleft.forEach(function(element){
+if (containerleft) {
+  containerleft.forEach(function (element) {
     element.classList.add('magictime', 'tinLeftIn', 'zindex')
-})}
+  })
+}
 
 const containerright = document.querySelectorAll('.container.right')
-if (containerright) 
-{
-  containerright.forEach(function(element){
+if (containerright) {
+  containerright.forEach(function (element) {
     element.classList.add('magictime', 'tinRightIn', 'zindex')
-})}
+  })
+}
 
 const portfolio = document.querySelector('#portfolio')
 if (portfolio) portfolio.classList.add('magictime', 'spaceInUp')
@@ -76,4 +77,3 @@ if (hobbies) hobbies.classList.add('magictime', 'spaceInUp')
 
 const contact = document.querySelector('#section')
 if (contact) contact.classList.add('magictime', 'vanishIn')
-
